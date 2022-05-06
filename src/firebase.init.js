b5e1dc0b721e48f58888310ecbfb31a336c605ca
rezainiet <masudrezaog@gmail.com>
2022-05-06 12:35:33 +0600

    added dotenv file and implemented

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,20 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+require('dotenv').config()
 const firebaseConfig = {
-    apiKey: "AIzaSyAa_Dh4FI0Advz-Qn2HN4F2L3CzgxHuKPs",
-    authDomain: "macology-afc12.firebaseapp.com",
-    projectId: "macology-afc12",
-    storageBucket: "macology-afc12.appspot.com",
-    messagingSenderId: "1008109617306",
-    appId: "1:1008109617306:web:48edf7b6f38886ddcbdf23"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
